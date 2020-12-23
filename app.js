e391231f481893d6644425e7e8bcc22584faae7d
RadhikaRJ <radhika7002@gmail.com>
2020-12-23 15:58:23 +0530

    feat: handled lucky number input

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-btnref=document.querySelector("#result");
-outputref=document.querySelector("#output");
-var luckyNumref =document.querySelector("#luckynum");
+btnref = document.querySelector("#result");
+outputref = document.querySelector("#output");
+var luckyNumref = document.querySelector("#luckynum");
 
-btnref.addEventListener("click",clickHandler);
+btnref.addEventListener("click", clickHandler);
 
 
-function clickHandler(){
+function clickHandler() {
     var inputDate = document.getElementById("dateip").value;
-    var luckyNum=luckyNumref.value;
+    var checkLuckyNum = isNaN(luckyNumref.value);
+    if (checkLuckyNum == true) {
+        alert("You must enter a valid digit as your lucky number.");
+    } else {
+        var luckyNum = luckyNumref.value;
 
-    var [year,month,day]=inputDate.toString().split('-');
+        var [year, month, day] = inputDate.toString().split('-');
 
-    year=Number(year);
-    month=Number(month);
-    day=Number(day);
+        year = Number(year);
+        month = Number(month);
+        day = Number(day);
 
-    var sum=sumOfDigits(year)+sumOfDigits(month)+sumOfDigits(day);
-    var rem=Math.floor(sum%luckyNum);
-    if(rem==0){
-        outputref.innerText="Yes Buddy, Your birthday is lucky!";
-    }
-    else{
-        outputref.innerText="Oops! Your birthdate missed being lucky by "+rem+" days!";
+        var sum = sumOfDigits(year) + sumOfDigits(month) + sumOfDigits(day);
+        var rem = Math.floor(sum % luckyNum);
+        if (rem == 0) {
+            outputref.innerText = "Yes Buddy, Your birthday is lucky!";
+        } else {
+            outputref.innerText = "Oops! Your birthdate missed being lucky by " + rem + " days!";
+        }
     }
 
 }
 
-function sumOfDigits(num){
-   var sum=0;
-    
-    while(num!= 0){
-        var rem=num%10;
-        sum=sum+rem;
-        num=Math.floor(num/10);
+function sumOfDigits(num) {
+    var sum = 0;
+
+    while (num != 0) {
+        var rem = num % 10;
+        sum = sum + rem;
+        num = Math.floor(num / 10);
     }
     return sum;
 
